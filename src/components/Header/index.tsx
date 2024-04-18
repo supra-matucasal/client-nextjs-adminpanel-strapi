9f94ee298b8d304525805a216c4d465ec64dfcf2
supra-matucasal <m.casal@supraoracles.com>
2024-04-18 23:00:49 +0200

    Add new layout and header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
-const Header = ({ theme, label, title }: { theme: string, label: string, title: string }) => {
+const Header = ({ links }: { links: any[] }) => {
   return (
     <div className="text-center pt-24">
-      {label && (
-        <h2 className={`text-${theme} font-extrabold tracking-wide uppercase`}>
-          {label}
-        </h2>
+      {links && (
+        <ul className="flex justify-center">
+          {links.map((link, index) => (
+            <li key={index} className="mx-4">
+              <a href={link.href} className="text-blue-500">
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
       )}
 
-      {title && (
-        <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-          {title}
-        </p>
-      )}
     </div>
   );
 };
